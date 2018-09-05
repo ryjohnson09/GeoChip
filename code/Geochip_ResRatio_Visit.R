5e929d76d0da82e60c7e3ab353548bb04069820c
ryjohnson09 <johnsrc06@gmail.com>
2018-09-05 14:56:19 -0400

    Remove LOP PLA tx groups and non-matched samples

--- code/Geochip_ResRatio_Visit.R
+++ code/Geochip_ResRatio_Visit.R
@@ -15,6 +15,56 @@ geochip <- read_tsv("data/processed/Merged_Geochip_Tidy.tsv",
                     col_types = "ccicccccccd",
                     progress = TRUE)
 
+# ID_Decoder
+ID_decoder <- suppressWarnings(suppressMessages(read_csv("data/processed/ID_Decoder.csv")))
+treat <- suppressWarnings(suppressMessages(read_csv("data/processed/TrEAT_Clinical_Metadata_tidy.csv")))
+
+# Filter ID_decoder to only include samples in geochip
+ID_decoder <- ID_decoder %>%
+  filter(glomics_ID %in% geochip$glomics_ID)
+
+treat <- treat %>%
+  left_join(., ID_decoder, by = c("STUDY_ID" = "study_id"))
+
+
+### Remove samples from geochip that 
+### are in the LOP & PLA tx groups 
+
+LOP_PLA_samples <- treat %>%
+  filter(Treatment %in% c("LOP", "PLA")) %>%
+  pull(glomics_ID)
+
+geochip <- geochip %>%
+  filter(!glomics_ID %in% LOP_PLA_samples)
+
+rm(LOP_PLA_samples)
+
+
+##############################################
+### Only keep patients with matched visits ###
+##############################################
+
+
+# Get vector of patients that have matched visits
+matched_samples <- ID_decoder %>%
+  filter(visit_number %in% c(1,4,5)) %>%
+  count(study_id) %>%
+  filter(n == length(c(1,4,5))) %>%
+  pull(study_id)
+
+# Get glomics ID's that correspond to the patients
+# with matched samples
+matched_glomics <- ID_decoder %>%
+  filter(study_id %in% matched_samples) %>%
+  pull(glomics_ID)
+
+# Subset the geochip data
+geochip <- geochip %>%
+  filter(glomics_ID %in% matched_glomics)
+rm(matched_glomics, matched_samples)
+
+
+# Calculate RRs
 geochip_RR <- geochip %>%
   
   group_by(glomics_ID) %>%
@@ -27,6 +77,12 @@ geochip_RR <- geochip %>%
   # Remove any rows with NA in the signal category or Gene_category
   filter(!is.na(Signal_Relative_Abundance)) %>%
   filter(!is.na(Gene_category)) %>%
+  
+  
+  
+  # Remove samples that are unmatched
+
+  # Remove the PLA and   
   
   # Calculate mead, sd, and counts (n)
   group_by(Gene_category, glomics_ID, visit_number) %>%
@@ -82,29 +138,46 @@ geochip_RR <- geochip %>%
   mutate(CI95_41 = abs(1.96 * SE_RR_41)) %>%
   mutate(CI95_51 = abs(1.96 * SE_RR_51))
 
+# Make Tidy for plotting
+geochip_RR_tidy <- geochip_RR %>%
+  select(Gene_category, RR_41, RR_51, CI95_41, CI95_51) %>%
+  gather(key = RR_group, value = RR, -Gene_category, -CI95_41, -CI95_51) %>%
+  gather(key = CI_group, value = CI95, -Gene_category, -RR_group, -RR) %>%
+  mutate(keepers = ifelse(str_extract(RR_group, "..$") == str_extract(CI_group, "..$"), "yes", NA)) %>%
+  filter(!is.na(keepers)) %>%
+  select(-keepers, -CI_group)
+
 
 # Plot
-geochip_RR_plot <- ggplot(data = arrange(geochip_RR, desc(RR_41))) +
-  geom_vline(xintercept = 0, linetype = "dashed", size = 1) +
-  # 1 v 4
-  geom_point(aes(x = RR_41, y = Gene_category), size = 4, color = "black") +
-  geom_errorbarh(aes(xmin = RR_41 - CI95_41, xmax = RR_41 + CI95_41, y = Gene_category), 
-                 color = "black") +
-  # 1 v 5
-  geom_point(aes(x = RR_51, y = Gene_category), size = 4, color = "red") +
-  geom_errorbarh(aes(xmin = RR_51 - CI95_51, xmax = RR_51 + CI95_51, y = Gene_category), 
-                 color = "red") +
-  ggtitle("Response Ratio: Gene Categories by Visit\nBlack = Visit 4 vs Visit 1\nRed = Visit 5 vs Visit 1") +
-  ylab("Gene Category") +
-  xlab("Response Ratio") +
+dodge <- position_dodge(width = 0.75)
+
+geochip_RR_plot <- ggplot(data = geochip_RR_tidy) +
+  geom_hline(yintercept = 0, linetype = "dashed", size = 1) +
+  
+  # points and error bar
+  geom_point(aes(x = Gene_category, y = RR, color = RR_group), size = 4, position = dodge) +
+  geom_errorbar(aes(ymin = RR - CI95, ymax = RR + CI95, x = Gene_category, color = RR_group), position = dodge) +
+  
+  scale_color_manual(values = c("black", "red"), labels = c("Visit 1 vs 4", "Visit 1 vs 5")) +
+
+  labs(title = "Response Ratio",
+       subtitle = "Gene Category by Visit",
+       x = "Gene Category",
+       y = "Response Ratio",
+       color = "Visit Comparisons",
+       caption = "Only considering samples from patients that provided samples at all 3 time points") +
   theme_minimal() +
+  coord_flip() +
   theme(
     axis.title.x = element_text(size = 15),
     axis.title.y = element_text(size = 15),
     axis.text.x = element_text(size = 12),
     axis.text.y = element_text(size = 12),
-    plot.title = element_text(size = 16, face = "bold")
+    plot.title = element_text(size = 16, face = "bold", hjust = 0.5),
+    plot.subtitle = element_text(hjust = 0.5),
+    plot.caption = element_text(hjust = 0.5)
   )
 
 
-ggsave("results/figures/Geochip_RespRatio_Visit.png", height = 10, width = 7)
+
+ggsave("results/figures/Geochip_RespRatio_Visit.png", height = 9, width = 8)
